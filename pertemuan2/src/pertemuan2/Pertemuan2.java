274185b95f6da59639d5b2246d7f1c4f2c6fc4d8
Ryan2301010356 <denfalperbatasan03@gmail.com>
2025-03-11 21:56:35 +0800

    pertemuan 2

---
+++ pertemuan2/src/pertemuan2/Pertemuan2.java
@@ -0,0 +1,30 @@
+/*
+ .
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan2;
+
+/**
+ *
+ * @author Aryan Hidayat
+ * tgl:11 Maret 2025
+ */
+public class Pertemuan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "Aryan Hidayat";
+        String nim = "2301010356";
+        
+        System.out.println("pertemuan 2 java:");
+        System.out.println("-----------------");
+        System.out.println("Nama : "+nama);
+        System.out.println ("----------------");
+        System.out.println("NIM :"+nim);
+        System.out.printf("Nama saya %s dengan NIM %s saat ini sedang belajar pemrograman bahasa java",nama,nim);
+    }
+    
+}
